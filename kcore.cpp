240da2a2494563b9fb133430dd1ea0238e8ff594
Bezzam, Varun <varun.bezzam@gatech.edu>
2018-02-08 15:36:55 -0500

    Works end to end. Minor issues in ordering.

--- kcore.cpp
+++ kcore.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <fstream>
 #include <algorithm>
 #include <fcntl.h>
@@ -20,6 +21,22 @@ struct edge {
 
 // Memory mapped edge list
 edge *edgeList;
+
+// Utility function to print the entire memory mapped graph
+void printGraph(int EDGENUM) {
+        for(int i = 0; i < EDGENUM; i++) {
+                std::cout<<(edgeList + i)->src<<" "<<(edgeList + i)->tgt<<"\n";
+        }
+}
+
+// Utility function to print a given array
+template <class T>
+void printArray(T *arr, int n) {
+        for(int i = 0; i < n; i++) {
+        std::cout<<arr[i]<<" ";
+        }
+        std::cout<<"\n";
+}
 
 // Memory maps input file
 void createMemoryMap(char *fileName) {
@@ -43,24 +60,8 @@ int compareByEdges(const void * a, const void * b) {
         if ((edgeList + *(int *)a)->src > (edgeList + *(int *)b)->src) return 1;
 }
 
-// Utility function to print the entire memory mapped graph
-void printGraph(int EDGENUM) {
-        for(int i = 0; i < EDGENUM; i++) {
-                std::cout<<(edgeList + i)->src<<" "<<(edgeList + i)->tgt<<"\n";
-        }
-}
-
-// Utility function to print a given array
-template <class T>
-void printArray(T *arr, int n) {
-        for(int i = 0; i < n; i++) {
-        std::cout<<arr[i]<<" ";
-        }
-        std::cout<<"\n";
-}
-
 // Formats the graph by sorting it and tracing original indices in the graph
-void formatGraph(const int EDGENUM, int *originalIndices) {
+void formatGraph(int EDGENUM, int *originalIndices) {
         int *indices = new int[EDGENUM];
         for(int i = 0; i < EDGENUM; i++) {
                 indices[i] = i;
@@ -73,12 +74,199 @@ void formatGraph(const int EDGENUM, int *originalIndices) {
         delete [] indices;
 }
 
+// Finds the start and end indices  of each node in the graph
+void findStartAndEndIndices(int *start, int *end, int EDGENUM) {
+        int i;
+        int old = edgeList->src;
+        start[old] = 0;
+        for(i = 0; i < EDGENUM; i++) {
+                if((edgeList + i)->src != old) {
+                        end[old] = i - 1;
+                        old = (edgeList + i)->src;
+                        start[old] = i;
+                }
+        }
+        end[old] = i;
+}
+
+// Computes the degree of each node in the graph
+void findDegree(int *edgeLabels, int EDGENUM, int NODENUM, int *degree) {
+        std::fill_n(degree, NODENUM + 1, 0);
+        for(int i = 0; i < EDGENUM; i++) {
+                // If edge hasn't been deleted yet. An edge is considered deleted
+                // when it has been labeled.
+                if(edgeLabels[i] == -1) {
+                        degree[(edgeList + i)->src]++;
+                        degree[(edgeList + i)->tgt]++;
+                }
+        }
+        for(int i = 0; i < NODENUM + 1; i++) {
+                degree[i] /= 2;
+        }
+}
+
+// Finds the neighbors of any node in the graph
+std::vector <int> findNeighbors(int node, int *start, int *end, int *edgeLabels) {
+        std::vector <int> neighbors;
+        if(start[node] == 0 && end[node] == 0)
+                return neighbors;
+        else {
+                for(int i = start[node]; i <= end[node]; i++) {
+                        if(edgeLabels[i] == -1) {
+                                neighbors.push_back((edgeList + i)->tgt);
+                        }
+                }
+        }
+        return neighbors;
+}
+
+void core(int *start_indices, int *end_indices, int NODENUM, int EDGENUM, int *edgeLabels, int *deg) {
+        int * vert = new int[NODENUM + 1];
+        int * pos = new int[NODENUM + 1];
+        std::fill_n(vert, NODENUM + 1, 0);
+        std::fill_n(pos, NODENUM + 1, 0);
+        int md = *std::max_element(deg, deg + NODENUM + 1);
+        int * bins = new int[md];
+        std::fill_n(bins, md, 0);
+        for(int v = 1; v <= NODENUM; v++)
+                bins[deg[v]]++;
+        int start = 1;
+        for(int d = 0; d <= md; d++) {
+                int num = bins[d];
+                bins[d] = start;
+                start += num;
+        }
+        for(int v = 1; v <= NODENUM; v++) {
+                pos[v] = bins[deg[v]];
+                vert[pos[v]] = v;
+                bins[deg[v]]++;
+        }
+        for(int d = md; d > 0; d--) {
+                bins[d] = bins[d - 1];
+        }
+        bins[0] = 1;
+        for(int i = 1; i <= NODENUM; i++) {
+                int v = vert[i];
+                std::vector <int> neighbors = findNeighbors(v, start_indices, end_indices, edgeLabels);
+                        for(std::vector <int> ::iterator i = neighbors.begin(); i != neighbors.end(); i++) {
+                                int u = *i;
+                                if(deg[u] > deg[v]) {
+                                        int du = deg[u];
+                                        int pu = pos[u];
+                                        int pw = bins[du];
+                                        int w = vert[pw];
+                                        if(u != w) {
+                                                pos[u] = pw;
+                                                pos[w] = pu;
+                                                vert[pu] = w;
+                                                vert[pw] = u;
+                                        }
+                                        bins[du]++;
+                                        deg[u]--;
+                                }
+                        }
+        }
+        delete [] vert;
+        delete [] pos;
+        delete [] bins;
+}
+
+void labelAndDeleteEdges(bool *isFinalNode, int peel, int EDGENUM, int *edgeLabels) {
+        for(int i = 0; i < EDGENUM; i++) {
+                if(isFinalNode[(edgeList + i)->src] && isFinalNode[(edgeList + i)->tgt] && edgeLabels[i] == -1) {
+                        edgeLabels[i] = peel;
+                }
+        }
+}
+
+bool isGraphEmpty(int *edgeLabels, int EDGENUM) {
+        for(int i = 0; i < EDGENUM; i++) {
+                if(edgeLabels[i] == -1)
+                        return false;
+        }
+        return true;
+}
+
+void writeToFile(int *edgeLabels, int EDGENUM, char *fileName) {
+        std::ofstream outputFile;
+        outputFile.open(fileName);
+        for(int i = 0; i < EDGENUM; i++) {
+                outputFile<<edgeLabels[i]<<"\n";
+        }
+        outputFile.close();
+}
+
+void doubleAndReverseGraph(char *inputFile, char *outputFile, int EDGENUM) {
+        std::ifstream is;
+        is.open(inputFile, std::ios::in | std::ios::binary);
+        std::ofstream os;
+        os.open(outputFile, std::ios::out | std::ios::binary | std::ios::app);
+        int src, tgt;
+        for(int i = 0; i < EDGENUM; i++) {
+                is.read((char *)(&src), sizeof(int));
+                is.read((char *)(&tgt), sizeof(int));
+                src = htonl(src);
+                tgt = htonl(tgt);
+                os.write((char *)&src, sizeof(int));
+                os.write((char *)&tgt, sizeof(int));
+        }
+        is.seekg(0, std::ios::beg);
+        for(int i = 0; i < EDGENUM; i++) {
+                is.read((char *)(&src), sizeof(int));
+                is.read((char *)(&tgt), sizeof(int));
+                src = htonl(src);
+                tgt = htonl(tgt);
+                os.write((char *)(&tgt), sizeof(int));
+                os.write((char *)(&src), sizeof(int));
+        }
+        is.close();
+        os.close();
+}
+
 int main(int argc, char *argv[]) {
-        const int EDGENUM = atoi(argv[2]);
-        const int NODENUM = atoi(argv[3]);
+        char *tmpFile = "tmp.bin";
+        remove(tmpFile);
+        int EDGENUM = atoi(argv[2]);
+        int NODENUM = atoi(argv[3]);
+        doubleAndReverseGraph(argv[1],tmpFile,  EDGENUM);
+        EDGENUM *= 2;
         int *originalIndices = new int[EDGENUM];
-        createMemoryMap(argv[1]);
+        int *edgeLabels = new int[EDGENUM];
+        std::fill_n(edgeLabels, EDGENUM, -1);
+        createMemoryMap(tmpFile);
         formatGraph(EDGENUM, originalIndices);
-        delete[] originalIndices;
+        int *start = new int[NODENUM + 1];
+        int *end = new int[NODENUM + 1];
+        std::fill_n(start, NODENUM + 1, 0);
+        std::fill_n(end, NODENUM + 1, 0);
+        findStartAndEndIndices(start, end, EDGENUM);
+        while(!isGraphEmpty(edgeLabels, EDGENUM)) {
+                int *degree = new int[NODENUM + 1];
+                findDegree(edgeLabels, EDGENUM, NODENUM, degree);
+                core(start, end, NODENUM, EDGENUM, edgeLabels, degree);
+                int mc = *std::max_element(degree, degree + NODENUM + 1);
+                bool *isFinalNode = new bool[NODENUM];
+                std::fill_n(isFinalNode, NODENUM, false);
+                for(int i = 0; i <= NODENUM; i++) {
+                        if(degree[i] == mc) {
+                                isFinalNode[i] = true;
+                        }
+                }
+                labelAndDeleteEdges(isFinalNode, mc, EDGENUM, edgeLabels);
+                delete [] degree;
+                delete [] isFinalNode;
+        }
+        EDGENUM /= 2;
+        int *originalLabels = new int[EDGENUM];
+        for(int i = 0; i < EDGENUM; i++) {
+                originalLabels[i] = edgeLabels[originalIndices[i]];
+        }
+        writeToFile(originalLabels, EDGENUM, argv[4]);
+        remove(tmpFile);
+        delete [] originalLabels;
+        delete [] edgeLabels;
+        delete [] start;
+        delete [] end;
+        delete [] originalIndices;
         return 0;
 }
